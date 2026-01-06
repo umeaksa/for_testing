1719e0c0b3f25b63a05f3dd85c2e000e38b25be0
Aqsa <umeaksa@gmail.com>
2026-01-06 21:36:21 +0500

    matrix

---
+++ addition of two matrices.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+	int a[2][2],b[2][2],c[2][2];
+	for(int i=0;i<2;i++)
+	for(int j=0;j<2;j++)
+	{
+		scanf("%d",&a[2][2]);
+		scanf("%d",&b[2][2]);
+		c[i][j]=a[i][j]+b[i][j];
+	}
+	return 0;
+}
